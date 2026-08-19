c3c19b198bf64c70972dedcf545b7b715064bfb8
Vítor Oliveira Amorim <voamorim345@gmail.com>
2026-08-19 20:08:29 -0300

    Integrate matplotlib for graph visualization
    Added visualization of the graph using matplotlib.

--- Lista2_CondaGit/main.py
+++ Lista2_CondaGit/main.py
@@ -1,6 +1,7 @@
 from label_propagation import LabelPropagation
 
 import networkx as nx
+import matplotlib.pyplot as plt
 
 INPUT_FOLDER = 'Input'
 
@@ -10,6 +11,20 @@ INPUT_FILE3 = f'{INPUT_FOLDER}/zachary.csv'
 
 def run_test(input_file:str):
     lp =LabelPropagation()
-    lp.read_graph(input_file)
-    
+    labels = lp.run(100, input_file)
+
+    print('=-' * 30)
+    print(' ' * 8, end='')
+    print(f'Rede: {input_file}')
+    print('=-' * 30)
+    print(f'\nLabels: {labels}\n')
+
+    fig, ax = plt.subplots(figsize=(6, 6))
+    nx.draw(lp.graph, nx.spring_layout(lp.graph), node_color=list(lp.labels.values()), cmap=plt.cm.Set1, with_labels=True, ax=ax) 
+    ax.set_title(f'Rede: {input_file}', fontsize=16)
+    plt.show()
+
+
 run_test(INPUT_FILE1)
+run_test(INPUT_FILE2)
+run_test(INPUT_FILE3)
